d7ab7c87b13c5168db89bfd212249187d3c5c734
Brian Lough <ecenuig@gmail.com>
2014-02-01 20:15:23 +0000

    Updating project with good name

---
+++ desktop/src/main/java/com/ladinc/java/CarPartADesktop.java
@@ -0,0 +1,14 @@
+package com.ladinc.java;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import com.ladinc.core.CarPartA;
+
+public class CarPartADesktop {
+	public static void main (String[] args) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.useGL20 = true;
+		new LwjglApplication(new CarPartA(), config);
+	}
+}
